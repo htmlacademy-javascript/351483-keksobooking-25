c457a4f4a47adccf020e683d2e1fcee450061d96
Keks <keks@htmlacademy.ru>
2022-02-19 19:00:24 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,22 +1,21 @@
 // function get random number from two integers number
 
 function getRandomNumber(min, max) {
-  let randomNumber;
   if (min < 0 || max < 0) {
-    throw new Error('Числа должны быть больше или равны 0')
-  };
+    throw new Error('Числа должны быть больше или равны 0');
+  }
   if (min === max) {
     return min;
-  };
+  }
   if (min > max) {
     const t = max;
     max = min;
     min = t;
-  };
-  randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 
   return randomNumber;
-};
+}
 
 getRandomNumber(0,3);
 
@@ -26,19 +25,18 @@ getRandomNumber(0,3);
 
 function getFloatingPointNumber(min, max, floatPoint) {
   if (min < 0 || max < 0 || floatPoint < 0) {
-    throw new Error('Числа должны быть больше или равны 0')
-  };
+    throw new Error('Числа должны быть больше или равны 0');
+  }
   if (min === max) {
     return min;
-  };
+  }
   if (min > max) {
     const t = max;
     max = min;
     min = t;
-  };
+  }
 
   return Number((Math.random() * (max - min) + min).toFixed(floatPoint));
-};
+}
 
 getFloatingPointNumber(1,11,3);
-
