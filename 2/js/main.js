49664410844855f4a6a8ed9ebf7116adec671f1a
Keks <keks@htmlacademy.ru>
2022-02-19 17:13:14 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -4,19 +4,19 @@ function getRandomNumber(min, max) {
   let randomNumber;
   if (min < 0 || max < 0) {
     throw new Error('Числа должны быть больше или равны 0')
-  }
+  };
   if (min === max) {
     return min;
-  }
+  };
   if (min > max) {
-    let t = max;
+    const t = max;
     max = min;
     min = t;
-  }
+  };
   randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 
   return randomNumber;
-}
+};
 
 getRandomNumber(0,3);
 
@@ -25,21 +25,20 @@ getRandomNumber(0,3);
 // and number of digits after the decimal point
 
 function getFloatingPointNumber(min, max, floatPoint) {
-  let randomNumber;
   if (min < 0 || max < 0 || floatPoint < 0) {
     throw new Error('Числа должны быть больше или равны 0')
-  }
+  };
   if (min === max) {
     return min;
-  }
+  };
   if (min > max) {
-    let t = max;
+    const t = max;
     max = min;
     min = t;
-  }
+  };
 
   return Number((Math.random() * (max - min) + min).toFixed(floatPoint));
-}
+};
 
 getFloatingPointNumber(1,11,3);
 
